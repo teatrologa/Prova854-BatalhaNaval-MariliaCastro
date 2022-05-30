bdecb4f97a7ffcfe27fa63d103f556578e78ba09
teatrologa <li.castrinho@gmail.com>
2022-05-30 17:08:13 -0300

    Corrigindo x1 e x2.

--- Prova854 - Batalha Naval - Marília Castro/Batalha Naval.cs
+++ Prova854 - Batalha Naval - Marília Castro/Batalha Naval.cs
@@ -472,11 +472,13 @@ public class BatalhaNaval
         var posicaoCortada = regexCorte.Matches(posicao).ToArray();
         var posicaoInicial = posicaoCortada[0].Value;
         var posicaoFinal = posicaoCortada[1].Value;
+        var posicaoXInicial = Convert.ToInt32(posicaoInicial[0]);
+        var posicaoXFinal = Convert.ToInt32(posicaoFinal[0]);
 
-        var x1 = Convert.ToInt32(posicaoInicial.Substring(0, 1)) - 65;
+        var x1 = posicaoXInicial - 65;
         var y1 = Int32.Parse(posicaoInicial.Substring(1));
 
-        var x2 = Convert.ToInt32(posicaoFinal.Substring(0, 1)) - 65;
+        var x2 = posicaoXFinal - 65;
         var y2 = Int32.Parse(posicaoFinal.Substring(1));
 
         if (x2 != x1 && y2 != y1)
